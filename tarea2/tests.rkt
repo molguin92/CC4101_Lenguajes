2a0cf8d5da13a40d3bf0b8c51c4932f125da9639
Manuel Olguin <molguin@dcc.uchile.cl>
2016-05-20 15:52:37 -0400

    beginning t2

--- tarea2/tests.rkt
+++ tarea2/tests.rkt
@@ -4,63 +4,63 @@
 ;; Test sub-module.
 ;; See http://blog.racket-lang.org/2012/06/submodules.html
 
-;this tests should never fail; these are tests for MiniScheme+ 
+;this tests should never fail; these are tests for MiniScheme+
 (module+ test
   (test (run '{+ 1 1}) 2)
-  
-  (test (run '{{fun {x y z} {+ x y z}} 1 2 3}) 6)  
-  
+
+  (test (run '{{fun {x y z} {+ x y z}} 1 2 3}) 6)
+
   (test (run '{< 1 2}) #t)
-  
+
   (test (run '{local {{define x 1}}
                 x}) 1)
-  
+
   (test (run '{local {{define x 2}
                       {define y {local {{define x 1}} x}}}
                 {+ x x}}) 4)
-  
+
     (test (run '{{fun {x y z} {+ x y z}} 1 2 3}) 6)
-  
+
   (test (run '{with {{x 1} {y 2} {z 3}} {+ x y z}}) 6)
-  
+
   (test (run '{with {} {{fun {} 42}}}) 42)
-  
-  ;; datatypes  
+
+  ;; datatypes
   (test (run '{local {{datatype List {Empty} {Cons a b}}} {List? {Empty}}}) #t)
-  
+
   (test (run '{local {{datatype List {Empty} {Cons a b}}} {Empty? {Empty}}}) #t)
-  
+
   (test (run '{local {{datatype List {Empty} {Cons a b}}} {List? {Cons 1 2}}}) #t)
-  
+
   (test (run '{local {{datatype List {Empty} {Cons a b}}} {Cons? {Cons 1 2}}}) #t)
-  
+
   (test (run '{local {{datatype List {Empty} {Cons a b}}} {Empty? {Cons 1 2}}})
         #f)
-  
+
   (test (run '{local {{datatype List {Empty} {Cons a b}}} {Empty? {Empty}}}) #t)
-  
+
   (test (run '{local {{datatype List {Empty} {Cons a b}}} {Cons? {Empty}}})
-        #f)      
-  
+        #f)
+
   ;; match
   (test (run '{match 1 {case 1 => 2}}) 2)
-  
+
   (test (run '{match 2
                 {case 1 => 2}
-                {case 2 => 3}})             
+                {case 2 => 3}})
         3)
-  
+
   (test (run '{match #t {case #t => 2}}) 2)
-  
+
   (test (run '{match #f
                 {case #t => 2}
-                {case #f => 3}})             
+                {case #f => 3}})
         3)
 
   (test (run '{local {{datatype Nat
                                 {Zero}
                                 {Succ n}}
-                      {define pred {fun {n} 
+                      {define pred {fun {n}
                                         {match n
                                           {case {Zero} => {Zero}}
                                           {case {Succ m} => m}}}}}
@@ -69,23 +69,23 @@
   (test (run '{local {{datatype Nat
                                 {Zero}
                                 {Succ n}}
-                      {define pred {fun {n} 
+                      {define pred {fun {n}
                                         {match n
                                           {case {Zero} => {Zero}}
                                           {case {Succ m} => m}}}}}
                 {Succ? {pred {Succ {Succ {Zero}}}}}}) #t))
 
-;tests for extended MiniScheme+ 
+;tests for extended MiniScheme+
 #;(module+ sanity-tests
-    (test (run '{local {{datatype Nat 
-                  {Zero} 
+    (test (run '{local {{datatype Nat
+                  {Zero}
                   {Succ n}}
-                {define pred {fun {n} 
+                {define pred {fun {n}
                                {match n
                                  {case {Zero} => {Zero}}
                                  {case {Succ m} => m}}}}}
           {pred {Succ {Succ {Zero}}}}}) "{Succ {Zero}}")
-  
+
 (test (run
  `{local ,stream-lib
           {local {,ones ,stream-take}
@@ -103,8 +103,7 @@
                                {+ n m}}
                           ones
                           ones}}}})  "{list 2 2 2 2 2 2 2 2 2 2}")
-(test 
+(test
 (run `{local ,stream-lib
                {local {,stream-take ,merge-sort ,fibs ,stream-zipWith}
                  {stream-take 10 {merge-sort fibs fibs}}}})   "{list 1 1 1 1 2 2 3 3 5 5}"))
-
